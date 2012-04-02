6c69e3248d6aee8164d09404ce9749ddbb26aab4
Janko Luin <janko@luin.se>
2012-04-02 11:56:50 +0200

    Rename config.vim ➔ vimrc (that's what it is!)

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-ln -sf ~/dotfiles/vim/config.vim ~/.vimrc
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/ackrc ~/.ackrc
