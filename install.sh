4c3b0a822dc4cec1183a590f1492ab640d059473
Janko Luin <janko@luin.se>
2012-05-30 21:58:23 +0200

    Symlink ~/.vim when installing dotfiles (d'oh!)

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
