cf2c855f06d01466b595f374d806a6d81ad48dcb
Janko Luin <janko@luin.se>
2012-06-19 10:24:45 +0200

    Add .gitconfig

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/dotfiles/ackrc ~/.ackrc
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 
 git config --global core.excludesfile ~/dotfiles/.gitignore
