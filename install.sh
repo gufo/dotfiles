9ab7386198697a6864cbbd8bf90ab076d9eef630
Janko Luin <janko.luin@valtech.se>
2013-12-19 09:43:12 +0100

    Auto-install zsh-syntax-highlighting plugin

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/.config
 
+echo "Symlinking..."
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
@@ -14,6 +15,14 @@ ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 if [ -d /usr/share/X11/xkb/symbols ]; then
   echo "X11 detected, attempting to add se_sv_dvorak keymap."
   sudo ln -sf ~/dotfiles/xkb/se_sv_dvorak /usr/share/X11/xkb/symbols/se_sv_dvorak && setxkbmap se_sv_dvorak
+fi
+
+if [[ -d ~/.oh-my-zsh/custom && ! -d ~/.oh-my-zsh/custom/zsh-syntax-highlighting ]]; then
+  echo "Installing zsh-syntax-highlighting plugin..."
+  mkdir -p ~/.oh-my-zsh/custom/plugins
+
+  cd ~/.oh-my-zsh/custom/plugins
+  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 fi
 
 git config --global core.excludesfile ~/dotfiles/.gitignore
