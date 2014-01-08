cb7d2b2e764ccf678557b9a2268387255071950a
Janko Luin <janko@luin.se>
2014-01-08 10:37:24 +0100

    Split up aliases into separate subfiles

---
+++ zsh/ruby.zsh
@@ -0,0 +1,9 @@
+alias be='bundle exec'
+alias cap='bundle exec cap'
+alias cucumber='bundle exec cucumber'
+alias guard='bundle exec guard'
+alias rspec='bundle exec rspec'
+alias egs='RAILS_ENV=test bundle exec evergreen serve'
+alias rails='bundle exec rake'
+alias rails='bundle exec rails'
+
