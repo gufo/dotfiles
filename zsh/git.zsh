6dd5626e67af7591322581efb693c47b71b678ac
Janko Luin <janko@luin.se>
2018-10-01 08:28:00 +0200

    Revert to oh-my-zsh, part 1: aliases
    This reverts commit 6d97b9c87a350ce3c9598501ce18e99769ff0f22.

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,24 +1,21 @@
-# Often-used shorthands
 alias u='git status -bs'
 alias d='git diff'
 alias a='git add'
-alias gc='git commit -v'
-alias gca='git commit -a -v'
-alias gco='git checkout'
-
-# Log variants
-alias gl="git log --pretty=compact --graph --color --first-parent '@{upstream}...HEAD'"
+alias gl='git log --pretty=compact --graph --color --first-parent @{upstream}...HEAD'
 alias glm='git log --pretty=compact --graph --color --first-parent origin/master...HEAD'
 alias glp='git log --pretty=compact --graph --color --first-parent origin/production...origin/master'
-
-# Branching operations
-alias master='git checkout master'
-alias gb='git branch'
-
-# Interactions with remote
+alias gco='git checkout'
 alias gpo='git push origin'
+alias master='git checkout master'
 alias gro='git rebase @{upstream}'
 alias gri='git rebase --interactive --autosquash'
 alias gfe='git fetch -p'
-alias gup='git pull -r -p'
+alias ff='git merge --ff-only'
 alias publish-branch='git push -u origin `git name-rev HEAD 2> /dev/null | awk "{ print \\$2 }"`'
+
+# git-svn aliases
+alias gfs='git svn fetch'
+alias gps='git svn dcommit'
+alias grs='git svn rebase'
+alias grsi='git rebase --interactive --autosquash remotes/trunk'
+alias gls='git log --pretty=compact --graph --color --first-parent remotes/trunk...HEAD'
