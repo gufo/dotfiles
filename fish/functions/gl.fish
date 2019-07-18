49c605e4ddba9fa015778f5e587d06fe8070072f
Janko Luin <janko.luin@valtech.com>
2019-07-18 13:06:41 +0200

    Add proper escaping

--- fish/functions/gl.fish
+++ fish/functions/gl.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function gl --description 'alias gl=git log --pretty=compact --graph --color --first-parent @{upstream}...HEAD'
-	git log --pretty=compact --graph --color --first-parent @{upstream}...HEAD $argv;
+	git log --pretty=compact --graph --color --first-parent @\{upstream\}...HEAD $argv;
 end
